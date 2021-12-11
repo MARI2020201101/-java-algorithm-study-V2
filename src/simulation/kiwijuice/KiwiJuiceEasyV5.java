1daeafa1cb13f452d3afe264c1753f7ed62e2d34
MARI <mari20201101@gmail.com>
2021-12-11 18:03:54 +0900

    Learn kiwijuice execercise v5

---
+++ src/simulation/kiwijuice/KiwiJuiceEasyV5.java
@@ -0,0 +1,37 @@
+package simulation.kiwijuice;
+
+import java.util.Arrays;
+
+public class KiwiJuiceEasyV5 {
+    public static void main(String[] args) {
+
+        int[] capacities = {30,20,10};
+        int[] bottles = {10,5,5};
+        int[] fromId = {0,1,2};
+        int[] toId = {1,2,0};
+
+        int[] result = thePouring(capacities, bottles, fromId, toId);
+        System.out.println(Arrays.toString(result));
+
+        int[] capacities2 = {14,35,86,58,25,62};
+        int[] bottles2 = {6,34,27,38,9,60};
+        int[] fromId2 = {1,2,4,5,3,3,1,0};
+        int[] toId2 = {0,1,2,4,2,5,3,1};
+
+        int[] result2 = thePouring(capacities2, bottles2, fromId2, toId2);
+        System.out.println(Arrays.toString(result2));
+    }
+
+    public static int[] thePouring(int[] capacities , int[] bottles, int[] fromId, int[] toId){
+
+        for (int i = 0; i < fromId.length; i++) {
+            int totalJuice = bottles[fromId[i]] + bottles[toId[i]];
+            bottles[toId[i]] = Math.min(totalJuice, capacities[toId[i]]);
+            //총량과 capacity 중에 더 적은 양이 toIdBottle에 들어가게 되고
+            //fromIdBottle 에는 남은 양이 들어가게 된다.
+            bottles[fromId[i]] = totalJuice - bottles[toId[i]];
+            // 총량을 보존해야 한다.
+        }
+       return bottles;
+    }
+}
