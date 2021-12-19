1e732f920fe4f6cc5597de02a1e8eb517ed30fa2
MARI <mari20201101@gmail.com>
2021-12-19 16:02:39 +0900

    Learn union two graphs to one

--- src/graph/isconnect/UnionAll.java
+++ src/graph/isconnect/UnionAll.java
@@ -16,13 +16,24 @@ public class UnionAll {
         if(a==b) return false; //원래부터 같은 그룹이었던 경우.
 
         //a쪽을 더 큰 그룹으로 만들 계획이다.
-        //따라서 b그룹이 더 크다면 교체한다.
+        //따라서 b그룹이 더 크다면 더 큰 그룹의 루트가 a가 되도록 한다.
         //루트 노드의 값에는 속한 노드의 카운트를 음수로 한 값이 들어있다.
         if(uni[a] > uni[b]){
             a^=b;
             b^=a;
             a^=b;
         }
-        return false;
+        uni[a] = uni[a] + uni[b];
+        uni[b] = a; //부모노드의 인덱스를 넣어준다.
+
+        return true;
+    }
+
+    public static boolean isConnect(int a, int b){
+        return root(a)==root(b);
+    }
+
+    public static int size(int a){
+        return -uni[root(a)];
     }
 }
