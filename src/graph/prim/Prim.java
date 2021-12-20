f55590fc44eff2fa0e84fadf967fb53310a70ef7
MARI <mari20201101@gmail.com>
2021-12-20 18:32:05 +0900

    Learn prim algorithm

---
+++ src/graph/prim/Prim.java
@@ -0,0 +1,46 @@
+package graph.prim;
+
+public class Prim {
+    static int[][] edge;
+    static int[][] dist;
+    public static int prim(int v){
+        boolean[] check = new boolean[v];
+        int[] d = new int[v];
+        for (int i = 0; i < v; i++) {
+            d[i] = Integer.MAX_VALUE;
+        }
+        int ret = 0;
+        d[0] = 0;
+        check[0] = true;
+
+
+        for (int i = 1; i < v; i++) {
+            int now = 0;
+            int nowd = Integer.MAX_VALUE;
+
+            for (int j = 0; j < v; j++) {
+                if(nowd > d[j] && !check[now]){
+                    now = j;
+                    nowd = d[j];
+                }
+            }
+            if(nowd==Integer.MAX_VALUE) return 0;
+            check[now] = true;
+            ret+=nowd;
+
+            for (int j = 0; j < edge[now].length; j++) {
+                int next = edge[now][j];
+                int nextd = dist[now][j];
+
+                if(nextd < d[next]){
+                    d[next] = nextd;
+                }
+            }
+        }
+
+
+
+
+        return ret;
+    }
+}
