7fe87d04cc7a6d0a528285dd27958cf0a9c408be
MARI <mari20201101@gmail.com>
2021-12-23 08:32:49 +0900

    Practice max stones

--- src/advance/nottwo/NotTwo.java
+++ src/advance/nottwo/NotTwo.java
@@ -1,4 +1,25 @@
 package advance.nottwo;
 
 public class NotTwo {
+    public static void main(String[] args) {
+        maxStones(10 ,10);
+    }
+
+    public static int maxStones(int width, int height){
+        int i;int j;
+        int[][] board = new int[1005][1005];
+        int res = 0;
+
+        for (i = 2; i < height + 2 ; i++) {
+            for (j = 2;j< width + 2 ; j++){
+                if(board[i-2][j] == 0 && board[i][j-2] == 0){
+                    System.out.print("O");
+                    res++;
+                    board[i][j]=1;
+                }else System.out.print("X");
+            }
+            System.out.println();
+        }
+        return res;
+    }
 }
