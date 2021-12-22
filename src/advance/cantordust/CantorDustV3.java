005235b2a7e595c6d7e8495f876b6d7b24c0a247
MARI <mari20201101@gmail.com>
2021-12-22 16:35:50 +0900

    Learn cantor dust problem

--- src/advance/cantordust/CantorDustV3.java
+++ src/advance/cantordust/CantorDustV3.java
@@ -14,12 +14,27 @@ public class CantorDustV3 {
             for(y=0;y<N;++y)
                 if(pattern[x].charAt(y)=='X')
                     a[x] = b[y] = flag = true;
+        //경우의 수
+        // X X
+        // . X
+        // X .
+        // . .
 
-        for(k=0;k+M<=L;++k){
+        for(k=0;k+M<=L;++k){ //이런식으로 겹치는 처리가 가능... 우와앙.....
             for(x=0;x<M;++x)
-                if((C.charAt(k+x) == 'X')!=a[x]) break;
+                if((C.charAt(k+x) == 'X')!=a[x]) break; //가로로 옮겨가면서 체크!!
+            //가로 대각선으로 옮겨가면서 체크하게 된다!
                 if(x==M) ++p;
         }
+        for(k=0;k+N<=L;++k){
+            for(x=0;x<N;++x)
+                if((C.charAt(k+x) == 'X')!=a[x]) break; //세로로 옮겨가면서 체크!!
+            if(x==N) ++q;
+        }
+
+        if(flag) return p*q; //X를 포함하는 경우.
+        else return (p * (L-N+1) + (L-M+1) * q - p*q); //X를 포함하지 않는 경우.
+
     }
 
     public static String getstring(int times){
