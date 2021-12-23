903acd3e2e9efb921c26f1d8e0f7f92f76ba111b
MARI <mari20201101@gmail.com>
2021-12-23 17:52:04 +0900

    Create isprime method with sqrt

---
+++ src/math/isprime/IsPrime.java
@@ -0,0 +1,16 @@
+package math.isprime;
+
+public class IsPrime {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(11));
+        System.out.println(isPrime(18));
+    }
+
+    public static boolean isPrime(int n){
+        for(int i = 3; i < Math.sqrt(n) ; i +=2){
+            if(n%i==0) return false;
+        }
+            return true;
+    }
+}
